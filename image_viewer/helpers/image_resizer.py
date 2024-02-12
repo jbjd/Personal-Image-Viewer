f222f020251251f2163afc5c2f9bb712391e6a0f
jbjd <jimdemetris@yahoo.com>
2024-02-12 00:16:13 -0600

    Fix: issues with zoom

--- image_viewer/helpers/image_resizer.py
+++ image_viewer/helpers/image_resizer.py
@@ -28,9 +28,16 @@ class ImageResizer:
             else None
         )
 
-    def get_zoomed_image(self, image: Image, zoom_factor: float) -> PhotoImage | None:
-        """Resizes image using the provided zoom_factor.
+    def get_zoomed_image(self, image: Image, zoom_level: int) -> PhotoImage | None:
+        """Resizes image using the provided zoom_level.
         Returns None when max zoom reached"""
+        # scale zoom factor based on image size vs screen and zoom level
+        w, h = image.size
+        zoom_scale: int = 1 + int(
+            max(w / self.screen_width, h / self.screen_height) / 5
+        )
+        zoom_factor: float = 1 + (0.25 * zoom_scale * zoom_level)
+
         dimensions, interpolation = self.dimension_finder(
             *_scale_tuple(image.size, zoom_factor)
         )
@@ -41,12 +48,7 @@ class ImageResizer:
             and zoom_factor > self.ZOOM_MIN
         ):
             return None
-
-        return PhotoImage(
-            (image.convert("RGB") if image.mode[:3] != "RGB" else image).resize(
-                dimensions, interpolation
-            )
-        )
+        return PhotoImage(resize(image, dimensions, interpolation))
 
     def _get_jpeg_scale_factor(
         self, image_width: int, image_height: int
