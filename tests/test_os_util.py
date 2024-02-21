8d07505c54ac27261dee79764774dd5cfd187b9d
jbjd <jimdemetris@yahoo.com>
2024-02-20 18:26:23 -0600

    Improvement: Truncate . and .. in paths when renaming

--- tests/test_os_util.py
+++ tests/test_os_util.py
@@ -2,7 +2,7 @@ import os
 
 import pytest
 
-from image_viewer.util.os import clean_str_for_OS_path, walk_dir
+from image_viewer.util.os import clean_str_for_OS_path, walk_dir, truncate_path
 
 
 @pytest.mark.skipif(os.name != "nt", reason="Only relevant to Windows")
@@ -17,3 +17,20 @@ def test_walk_dir(img_dir: str):
     """Test that walk_dir behaves like expected"""
     files = [p for p in walk_dir(img_dir)]
     assert len(files) == 5
+
+
+def test_truncate_path():
+    # test truncating . to same directory
+    assert truncate_path("/./asdf") == "/asdf"
+    assert truncate_path("asdf/./123/.") == "asdf/123/"
+
+    # test truncating .. to previous directory
+    assert truncate_path("abc/123/..") == "abc/"
+    assert truncate_path("abc/123/../") == "abc/"
+    assert truncate_path("abc/./123/../.") == "abc/"
+
+
+@pytest.mark.skipif(os.name != "nt", reason="Only relevant to Windows")
+def test_truncate_path_nt():
+    assert truncate_path("C:/.\\asdf\\.\\") == "C:/asdf/"
+    assert truncate_path("C:/abc\\./123/..\\.") == "C:/abc/"
