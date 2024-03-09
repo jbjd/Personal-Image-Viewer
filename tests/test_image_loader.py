de902a82802655fce1fecf1045b701e796b3e7db
jbjd <jimdemetris@yahoo.com>
2024-03-08 18:25:50 -0600

    Test: More coverage for image_loader and file_manager

--- tests/test_image_loader.py
+++ tests/test_image_loader.py
@@ -1,13 +1,24 @@
+from unittest.mock import mock_open, patch
+
 import pytest
+from PIL import UnidentifiedImageError
+from PIL.Image import Image
 
 from image_viewer.helpers.image_loader import ImageLoader
 from image_viewer.helpers.image_resizer import ImageResizer
-from test_util.mocks import MockImage
+from image_viewer.util.image import CachedImage
+from test_util.mocks import (
+    MockImage,
+    MockImageFileManager,
+    MockPhotoImage,
+    MockStatResult,
+)
 
 
 @pytest.fixture
 def image_loader(image_resizer: ImageResizer) -> ImageLoader:
-    image_loader = ImageLoader(None, image_resizer, lambda *_: None)  # type: ignore
+    image_loader = ImageLoader(MockImageFileManager(), image_resizer, lambda *_: None)
+    image_loader.PIL_image = MockImage()
     return image_loader
 
 
@@ -42,8 +53,6 @@ def test_next_frame(image_loader: ImageLoader):
 
 def test_get_ms_until_next_frame(image_loader: ImageLoader):
     """Should get ms until next frame in animated images without erroring"""
-    image_loader.PIL_image = MockImage()
-
     # non-animated image gets default
     default_speed: int = image_loader.DEFAULT_ANIMATION_SPEED
     assert image_loader.get_ms_until_next_frame() == default_speed
@@ -53,3 +62,43 @@ def test_get_ms_until_next_frame(image_loader: ImageLoader):
 
     image_loader.PIL_image.info["duration"] = 66
     assert image_loader.get_ms_until_next_frame() == 66
+
+
+def test_load_image_error_on_open(image_loader: ImageLoader):
+    """An image might error on open when its not a valid image or not found"""
+
+    with patch("builtins.open", side_effect=FileNotFoundError()):
+        assert image_loader.load_image() is None
+
+    with patch("builtins.open", mock_open(read_data="abcd")):
+        with patch(
+            "image_viewer.helpers.image_loader.open_image",
+            side_effect=UnidentifiedImageError(),
+        ):
+            assert image_loader.load_image() is None
+
+
+@patch("builtins.open", mock_open(read_data="abcd"))
+@patch("image_viewer.helpers.image_loader.open_image", lambda *_: Image())
+def test_load_image_in_cache(image_loader: ImageLoader):
+    """When an image of the same name is in cache, don't load from disk"""
+
+    # setup cache for test
+    image_byte_size: int = 10
+    cached_image = MockPhotoImage()
+    cached_data = CachedImage(cached_image, 10, 10, "10kb", image_byte_size, "RGB")
+    image_loader.file_manager.get_current_image_cache = lambda: cached_data
+
+    mock_os_stat = MockStatResult(image_byte_size)
+    with patch("image_viewer.helpers.image_loader.stat", lambda _: mock_os_stat):
+        assert image_loader.load_image() is cached_image
+
+
+def test_load_image_resize_error(image_loader: ImageLoader):
+    """Should get placeholder image when resize errors"""
+    with patch.object(ImageResizer, "get_image_fit_to_screen", side_effect=OSError):
+        with patch(
+            "image_viewer.helpers.image_loader.get_placeholder_for_errored_image"
+        ) as mock_get_placeholder:
+            image_loader._load_image_from_disk_and_cache(0)
+            mock_get_placeholder.assert_called_once()
--- tests/test_util/mocks.py
+++ tests/test_util/mocks.py
@@ -3,34 +3,21 @@ from __future__ import annotations
 from tkinter import Event
 
 from PIL.Image import Image
+from PIL.ImageTk import PhotoImage
 
+from image_viewer.managers.file_manager import ImageFileManager
 from image_viewer.util.action_undoer import ActionUndoer
+from image_viewer.util.image import ImageName
 
 
-class MockImage(Image):
-    """Mocks PIL Image for testing"""
+class MockStatResult:
+    """Mocks OS's stat_result"""
 
-    mode: str = "P"
-    info: dict = {}
+    st_ctime: int = 1649709119
+    st_mtime: int = 1649709119
 
-    def __init__(self, n_frames: int = 1) -> None:
-        self.n_frames: int = n_frames
-
-        if n_frames > 1:  # Like PIL, only set for animtions
-            self.is_animated = True
-
-    def convert(self, new_mode: str) -> MockImage:
-        self.mode = new_mode
-        return self
-
-    def save(self, *_, **kwargs) -> None:
-        pass
-
-    def __enter__(self) -> MockImage:
-        return self
-
-    def __exit__(self, *_) -> None:
-        pass
+    def __init__(self, st_size: int) -> None:
+        self.st_size: int = st_size
 
 
 class MockEvent(Event):
@@ -40,8 +27,47 @@ class MockEvent(Event):
         self.x: int = x
 
 
+class MockImage(Image):
+    """Mocks PIL Image for testing"""
+
+    mode: str = "P"
+    info: dict = {}
+    _size: tuple[int, int] = (0, 0)
+
+    def __init__(self, n_frames: int = 1) -> None:
+        self.n_frames: int = n_frames
+
+        if n_frames > 1:  # Like PIL, only set for animtions
+            self.is_animated: bool = True
+
+    def convert(self, new_mode: str) -> MockImage:  # type: ignore
+        self.mode = new_mode
+        return self
+
+    def save(self, *_, **kwargs) -> None:
+        pass
+
+
+class MockPhotoImage(PhotoImage):
+    """Mocks PIL's PhotoImage"""
+
+    def __init__(self) -> None:
+        pass
+
+
 class MockActionUndoer(ActionUndoer):
     """Mocks this module's ActionUndoer"""
 
     def undo(*_):
         return ("b.png", "a.png")
+
+
+class MockImageFileManager(ImageFileManager):
+    """Mocks this module's ImageFileManager"""
+
+    cache: dict = {}
+    current_image = ImageName("test.png")
+    path_to_current_image: str = ""
+
+    def __init__(self) -> None:
+        pass
