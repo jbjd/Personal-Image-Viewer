39a666a927bc6ff0c3eb3391c708c1fcbffa72ec
jbjd <jimdemetris@yahoo.com>
2024-07-02 19:41:41 -0500

    Improvement: skip uneeded PIL code in standalone

--- compile_utils/cleaner.py
+++ compile_utils/cleaner.py
@@ -13,7 +13,6 @@ from compile_utils.code_to_skip import (
     classes_to_skip,
     dict_keys_to_skip,
     function_calls_to_skip,
-    functions_to_noop,
     functions_to_skip,
     regex_to_apply,
     vars_to_skip,
@@ -45,7 +44,6 @@ class CleanUnpsarser(ast._Unparser):  # type: ignore
         "vars_to_skip",
         "classes_to_skip",
         "func_calls_to_skip",
-        "func_to_noop",
     ]
 
     def __init__(self, module_name: str = "") -> None:
@@ -61,9 +59,6 @@ class CleanUnpsarser(ast._Unparser):  # type: ignore
         }
         self.func_calls_to_skip: dict[str, int] = {
             k: 0 for k in function_calls_to_skip[module_name]
-        }
-        self.func_to_noop: dict[str, int] = {
-            k: 0 for k in functions_to_noop[module_name]
         }
         self.dict_keys_to_skip: dict[str, int] = {
             k: 0 for k in dict_keys_to_skip[module_name]
@@ -89,12 +84,6 @@ class CleanUnpsarser(ast._Unparser):  # type: ignore
             self.func_to_skip[node.name] += 1
             return
 
-        if node.name in self.func_to_noop:
-            self.func_to_noop[node.name] += 1
-            node.body = [ast.Pass()]
-            super().visit_FunctionDef(node)
-            return
-
         argument: ast.arg
         for argument in node.args.args:
             argument.annotation = None
@@ -118,6 +107,7 @@ class CleanUnpsarser(ast._Unparser):  # type: ignore
         ):
             super().visit_Call(node)
         else:
+            super().visit_Pass(node)
             self.func_calls_to_skip[
                 getattr(node.func, "attr", "") or getattr(node.func, "id", "")
             ] += 1
@@ -210,7 +200,7 @@ def clean_file_and_copy(path: str, new_path: str, module_name: str = "") -> None
 
     parsed_source: ast.Module = ast.parse(source)
     code_cleaner = CleanUnpsarser(module_name)
-    contents: str = code_cleaner.visit(ast.NodeTransformer().visit(parsed_source))
+    source = code_cleaner.visit(ast.NodeTransformer().visit(parsed_source))
 
     # Check for code that was marked to ksip, but was not found in the module
     for var in code_cleaner.VARS_TRACKING_REMOVED:
@@ -225,18 +215,16 @@ def clean_file_and_copy(path: str, new_path: str, module_name: str = "") -> None
             )
 
     if autoflake is not None:
-        contents = autoflake.fix_code(
-            contents,
+        source = autoflake.fix_code(
+            source,
             remove_all_unused_imports=True,
             remove_duplicate_keys=True,
             remove_unused_variables=True,
             remove_rhs_for_unused_variables=True,
-            ignore_pass_statements=True,
-            ignore_pass_after_docstring=True,
         )
 
     with open(new_path, "w", encoding="utf-8") as fp:
-        fp.write(contents)
+        fp.write(source)
 
 
 def move_files_to_tmp_and_clean(
@@ -246,7 +234,7 @@ def move_files_to_tmp_and_clean(
     for python_file in iter(
         os.path.abspath(p)
         for p in glob(f"{dir}/**/*", recursive=True)
-        if p.endswith((".py", ".pyi", ".pyd"))
+        if p.endswith((".py", ".pyd"))
     ):
         if os.path.basename(python_file) == "__main__.py" and mod_prefix != "":
             continue  # skip __main__ in modules other than this one
