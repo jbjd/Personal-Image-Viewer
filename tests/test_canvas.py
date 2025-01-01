4dc9ac34c061bf2adfc21a05a2c59a4c55248ae6
jbjd <jimdemetris@yahoo.com>
2025-01-01 15:03:19 -0600

    Test: More coverage

--- tests/test_canvas.py
+++ tests/test_canvas.py
@@ -1,4 +1,8 @@
+from unittest.mock import patch
+
 import pytest
+from PIL.Image import Image
+from PIL.ImageTk import PhotoImage
 
 from image_viewer.constants import Key
 from image_viewer.ui.canvas import CustomCanvas
@@ -15,7 +19,7 @@ def right_key_event(tk_app):
     return MockEvent(widget=tk_app, keysym_num=Key.RIGHT)
 
 
-def test_create_assets(canvas: CustomCanvas):
+def test_create_assets(canvas: CustomCanvas, example_image: Image):
     """Ensure creation of buttons, text, and topbar goes well"""
 
     # Should store id after creation
@@ -24,17 +28,33 @@ def test_create_assets(canvas: CustomCanvas):
 
     assert canvas.update_file_name("new.png")
 
-    # Should store image after creation, normally a PhotoImage but a None for testing
-    canvas.create_topbar(None)  # type: ignore
-    assert canvas.topbar is None
+    # Should store image after creation or garbage collector kills topbar
+    display_image = PhotoImage(example_image)
+    canvas.create_topbar(display_image)
+    assert canvas.topbar is display_image
 
 
-def test_clean_long_name():
-    long_name: str = "too_long12" * 10 + ".png"
-    assert (
-        CustomCanvas._clean_long_name(long_name)
-        == "too_long12too_long12too_long12too_long(…).png"
-    )
+def test_update_image_display(canvas: CustomCanvas, example_image: Image):
+    """Should center images on update_image_display and
+    preserve coords on update_existing_image_display"""
+    display_image = PhotoImage(example_image)
+    canvas.update_image_display(display_image)
+    image_id: int = canvas.image_display.id
+
+    assert image_id != -1
+
+    # Move, update existing, and assert image was not moved
+    canvas.moveto(image_id, -1, -1)
+    original_coords: list[float] = canvas.coords(image_id)
+
+    canvas.update_existing_image_display(display_image)
+    assert image_id == canvas.image_display.id
+    assert original_coords == canvas.coords(image_id)
+
+    # When updating a new image, should re-center
+    canvas.update_image_display(display_image)
+    assert image_id != canvas.image_display.id
+    assert original_coords != canvas.coords(image_id)
 
 
 def test_widget_visible(canvas: CustomCanvas):
@@ -44,3 +64,38 @@ def test_widget_visible(canvas: CustomCanvas):
 
     canvas.itemconfigure(widget_id, state="hidden")
     assert not canvas.is_widget_visible(widget_id)
+
+
+@pytest.mark.parametrize(
+    "start_coords,end_coords,expected_move_amount",
+    [
+        ((0, 0), (10, 10), (10, 10)),
+        ((0, 0), (9999, 9999), (1920, 1080)),
+        ((0, 0), (-99, -99), (-10, -10)),  # example_image is 10x10
+    ],
+)
+def test_drag(
+    canvas: CustomCanvas,
+    example_image: Image,
+    start_coords: tuple[int, int],
+    end_coords: tuple[int, int],
+    expected_move_amount: tuple[int, int],
+):
+    """Should move image amount dragged"""
+    canvas.screen_width = 1920
+    canvas.screen_height = 1080
+    canvas.update_image_display(PhotoImage(example_image))
+
+    start_x, start_y = start_coords
+    event_origin = MockEvent(x=start_x, y=start_y)
+    end_x, end_y = end_coords
+    event_end = MockEvent(x=end_x, y=end_y)
+
+    canvas.moveto(canvas.image_display.id, 0, 0)
+    with patch.object(CustomCanvas, "move") as mock_move:
+        canvas._move_from(event_origin)
+        canvas._move_to(event_end)
+
+        mock_move.assert_called_once_with(
+            canvas.image_display.id, *expected_move_amount
+        )
