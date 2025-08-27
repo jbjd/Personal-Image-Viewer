062f123de6d9ca1fcb4b6ddf715bc69d88dd76e2
jbjd <jimdemetris@yahoo.com>
2025-08-26 22:33:43 -0500

    Typo

--- image_viewer/image/loader.py
+++ image_viewer/image/loader.py
@@ -132,10 +132,10 @@ class ImageLoader:
             return None
 
         original_image: Image = read_image_response.image
-        byte_size: int = self.image_buffer.byte_size
+        byte_size: int = read_image_response.image_buffer.byte_size
 
-        self.PIL_image = original_image
         self.image_buffer = read_image_response.image_buffer
+        self.PIL_image = original_image
         self.current_load_id += 1
 
         # check if cached and not changed outside of program
