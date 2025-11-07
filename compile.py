57c36fe6f3415037a71d2346723db443a415956e
jbjd <jimdemetris@yahoo.com>
2025-11-06 22:47:33 -0600

    Simplify compile step

--- compile.py
+++ compile.py
@@ -2,13 +2,11 @@
 
 import os
 import sys
-from importlib import import_module
 from importlib.metadata import version as get_module_version
 from subprocess import Popen
 
 from personal_compile_tools.file_operations import (
     copy_file,
-    copy_folder,
     delete_file,
     delete_folder,
     delete_folders,
@@ -16,20 +14,10 @@ from personal_compile_tools.file_operations import (
 )
 
 from compile_utils.args import CompileArgumentParser, CompileNamespace, NuitkaArgs
-from compile_utils.cleaner import (
-    clean_file_and_copy,
-    clean_tk_files,
-    move_files_to_tmp_and_clean,
-    strip_files,
-    warn_unused_code_skips,
-)
+from compile_utils.cleaner import clean_tk_files, strip_files
 from compile_utils.constants import BUILD_INFO_FILE, IMAGE_VIEWER_NAME
 from compile_utils.log import setup_logging
-from compile_utils.module_dependencies import (
-    get_normalized_module_name,
-    module_dependencies,
-    modules_to_skip,
-)
+from compile_utils.module_dependencies import module_dependencies, modules_to_skip
 from compile_utils.nuitka_ext import start_nuitka_compilation
 from compile_utils.validation import (
     raise_if_not_root,
@@ -76,56 +64,9 @@ _logger = setup_logging()
 
 validate_module_requirements()
 
-delete_folder(TMP_FOLDER)
-os.makedirs(TMP_FOLDER)
 try:
-    clean_file_and_copy(
-        f"{WORKING_FOLDER}/{TARGET_FILE}",
-        f"{TMP_FOLDER}/{TARGET_FILE}",
-        IMAGE_VIEWER_NAME,
-        f"{IMAGE_VIEWER_NAME}.{TARGET_MODULE}",
-    )
-    move_files_to_tmp_and_clean(CODE_FOLDER, TMP_FOLDER, IMAGE_VIEWER_NAME)
-
-    for module in module_dependencies:
-        module_name: str = get_normalized_module_name(module)
-        sub_modules_to_skip: set[str] = set(
-            i for i in modules_to_skip if i.startswith(module_name)
-        )
-
-        module = import_module(module_name)
-        if module.__file__ is None:
-            raise ValueError(f"Module {module_name}'s file path not set")
-
-        base_file_name: str = os.path.basename(module.__file__)
-
-        if module_name == "PIL" and os.name != "nt":
-            site_packages_path = os.path.dirname(os.path.dirname(module.__file__))
-            lib_path = os.path.join(site_packages_path, "pillow.libs")
-            copy_folder(lib_path, os.path.join(TMP_FOLDER, "pillow.libs"))
-
-        if base_file_name == "__init__.py":  # Its a folder
-            move_files_to_tmp_and_clean(
-                os.path.dirname(module.__file__),
-                TMP_FOLDER,
-                module_name,
-                sub_modules_to_skip,
-            )
-        else:  # Its one file
-            clean_file_and_copy(
-                module.__file__,
-                os.path.join(TMP_FOLDER, base_file_name),
-                module_name,
-                module_name,
-            )
-
-    warn_unused_code_skips()
-
-    if args.skip_nuitka:
-        sys.exit(0)
-
     delete_folder(COMPILE_FOLDER)
-    target_file_path: str = f"{TMP_FOLDER}/{TARGET_FILE}"
+    target_file_path: str = f"{WORKING_FOLDER}/{TARGET_FILE}"
     default_python: str = "python" if os.name == "nt" else "bin/python3"
     python_path: str = os.path.join(sys.exec_prefix, default_python)
     process: Popen = start_nuitka_compilation(
@@ -144,18 +85,6 @@ try:
         new_path = os.path.join(COMPILE_FOLDER, data_file_path)
         os.makedirs(os.path.dirname(new_path), exist_ok=True)
         copy_file(old_path, new_path)
-
-    if args.build_info_file:
-        with open(
-            os.path.join(COMPILE_FOLDER, BUILD_INFO_FILE), "w", encoding="utf-8"
-        ) as fp:
-            fp.write(f"OS: {os.name}\n")
-            fp.write(f"Python: {sys.version}\n")
-            fp.write("Dependencies:\n")
-            for module in module_dependencies:
-                name: str = module.name
-                fp.write(f"\t{name}: {get_module_version(name)}\n")
-            fp.write(f"Arguments: {args}\n")
 
     clean_tk_files(COMPILE_FOLDER)
     if args.strip:
