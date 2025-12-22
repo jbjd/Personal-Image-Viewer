7ca85b5d8f1f22d8acaac05e6cdd43f9000d9e30
jbjd <jimdemetris@yahoo.com>
2025-12-21 21:50:17 -0600

    Bump: personal-compile-tools to 1.3.0

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,6 +1,6 @@
 """Tests for generic exception handling code."""
 
-from unittest.mock import mock_open, patch
+from unittest.mock import MagicMock, mock_open, patch
 
 from image_viewer.exceptions import exception_hook
 
@@ -14,6 +14,7 @@ def test_exception_hook():
         # Should catch and not fail writing to file
         exception_hook(type(exception), exception, None, "")
 
+    mock_builtins_open: MagicMock
     with patch("builtins.open", mock_open()) as mock_builtins_open:
         exception_hook(type(exception), exception, None, "")
         mock_builtins_open.assert_called_once_with("ERROR.log", "w", encoding="utf-8")
