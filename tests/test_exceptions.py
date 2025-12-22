8cbf94678e9c72b82e0ca507f4c444190eca7e6b
jbjd <jimdemetris@yahoo.com>
2025-12-21 21:27:39 -0600

    Bump: personal-compile-tools to 1.2.0

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -14,6 +14,6 @@ def test_exception_hook():
         # Should catch and not fail writing to file
         exception_hook(type(exception), exception, None, "")
 
-    with patch("builtins.open", new_callable=mock_open) as mock_builtins_open:
+    with patch("builtins.open", mock_open()) as mock_builtins_open:
         exception_hook(type(exception), exception, None, "")
         mock_builtins_open.assert_called_once_with("ERROR.log", "w", encoding="utf-8")
