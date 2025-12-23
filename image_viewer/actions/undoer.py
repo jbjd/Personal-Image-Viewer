2df126397eebff95ea33a45c97b90fcf4a813bac
jbjd <jimdemetris@yahoo.com>
2025-12-22 23:14:19 -0600

    Docs in action_undoer

--- image_viewer/actions/undoer.py
+++ image_viewer/actions/undoer.py
@@ -10,49 +10,47 @@ from image_viewer.util.os import restore_file, trash_file
 
 
 class UndoResponse:
-    """Response when a file action in undone, contains what paths have been edited
-    so caller can deal with determining if display needs updating"""
+    """Contains what paths where restored or removed, if any,
+    when an action was undone."""
 
-    __slots__ = ("path_to_restore", "path_to_remove")
+    __slots__ = ("path_restored", "path_removed")
 
-    def __init__(self, path_to_restore: str, path_to_remove: str) -> None:
-        self.path_to_restore: str = path_to_restore
-        self.path_to_remove: str = path_to_remove
+    def __init__(self, path_restored: str, path_removed: str) -> None:
+        self.path_restored: str = path_restored
+        self.path_removed: str = path_removed
 
 
 class ActionUndoer(deque[FileAction]):
-    """Keeps information on recent file actions and can undo them"""
+    """Keeps track of recent file actions and can undo them."""
 
     __slots__ = ()
 
-    def __init__(self, max_length: int = 4) -> None:
+    def __init__(self, max_length: int = 8) -> None:
         super().__init__(maxlen=max_length)
 
     def undo(self) -> UndoResponse:
-        """Returns response with image to restore and/or remove based on action
-        being undone"""
+        """Undoes most recent action. Files will be renamed, trashed, and or restored
+        from trash to match the expected file state before the action occured.
+
+        :returns: An UndoResponse with the restored/removed file paths if any."""
         # pylint: disable=unidiomatic-typecheck
 
         action: FileAction = self.pop()
 
         if type(action) is Rename:
-
             os.rename(action.new_path, action.original_path)
             return UndoResponse(action.original_path, action.new_path)
 
         if type(action) is Convert and action.original_file_deleted:
-
             restore_file(action.original_path)
             trash_file(action.new_path)
             return UndoResponse(action.original_path, action.new_path)
 
         if type(action) is Convert:
-
             trash_file(action.new_path)
             return UndoResponse("", action.new_path)
 
         if type(action) is Delete:
-
             restore_file(action.original_path)
             return UndoResponse(action.original_path, "")
 
