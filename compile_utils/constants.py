2a9b98dfded65d09d3ace5570c067b1463ab13f7
James D <63110925+jbjd@users.noreply.github.com>
2025-12-29 19:01:31 -0600

    Cleanup: Address pylint and update GHA versions to latest (#60)

--- compile_utils/constants.py
+++ compile_utils/constants.py
@@ -1,15 +1,11 @@
-"""Constant values used during compilation, such as file names"""
+"""Constant values used during compilation."""
 
 # Project
 
 IMAGE_VIEWER_NAME: str = "image_viewer"
 PROJECT_FILE: str = "pyproject.toml"
 
-# Compilation
+# File Names
 
 BUILD_INFO_FILE: str = "build_info.txt"
 REPORT_FILE: str = "compilation-report.xml"
-
-# Logging
-
-LOGGER_NAME: str = "personal_logger"
