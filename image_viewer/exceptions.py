3549d89b0afeb7b1b55720b7367b654ed3305ac2
jbjd <jimdemetris@yahoo.com>
2026-01-15 17:24:45 -0600

    Fix exception_hook

--- image_viewer/exceptions.py
+++ image_viewer/exceptions.py
@@ -7,16 +7,12 @@ def exception_hook(
     error_type: type[BaseException],
     error: BaseException,
     trace: TracebackType | None,
-    destination_folder: str,
 ) -> None:
     """Writes unhandled fatal exception to file."""
-    import os
     import traceback
 
-    error_file: str = os.path.join(destination_folder, "ERROR.log")
-
     try:  # Try to write, but don't allow another exception since that may be confusing
-        with open(error_file, "w", encoding="utf-8") as fp:
+        with open("ERROR.log", "w", encoding="utf-8") as fp:
             traceback.print_exception(error_type, error, trace, file=fp)
     except OSError:
         pass
