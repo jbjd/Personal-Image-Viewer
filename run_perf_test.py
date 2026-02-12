e86f2a962dea00b34c9361b6c27f16819c7dd58f
jbjd <jimdemetris@yahoo.com>
2026-02-12 17:38:20 -0600

    Fic codespell, remove clean step

--- run_perf_test.py
+++ run_perf_test.py
@@ -14,7 +14,7 @@ if len(sys.argv) < 2 or sys.argv[1] not in perf_tests:
         print("Invalid argument", sys.argv[1])
         exit_code = 1
 
-    print("Runs a performace test, one of:", perf_tests)
+    print("Runs a performance test, one of:", perf_tests)
     sys.exit(exit_code)
 
 perf_module: ModuleType = importlib.import_module("perf." + sys.argv[1])
