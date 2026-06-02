0586431625fe0d3f1b27bccbc17435594bd9c2a0
James D <63110925+jbjd@users.noreply.github.com>
2026-06-01 21:35:41 -0500

    Refactor: Add .clang-format (#86)

--- image_viewer/c_extensions/b64/cencode.c
+++ image_viewer/c_extensions/b64/cencode.c
@@ -9,23 +9,21 @@ Taken from https://github.com/BuLogics/libb64/tree/master with slight edits
 
 #include "includes/cencode.h"
 
-void base64_init_encodestate(base64_encodestate *state_in)
-{
+void base64_init_encodestate(base64_encodestate *state_in) {
     state_in->step = step_A;
     state_in->result = 0;
     state_in->stepcount = 0;
 }
 
-char base64_encode_value(char value_in)
-{
+char base64_encode_value(char value_in) {
     static const char *encoding = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    if (value_in > 63)
+    if (value_in > 63) {
         return '=';
+    }
     return encoding[(int)value_in];
 }
 
-int base64_encode_block(const char *plaintext_in, unsigned int length_in, char *code_out, base64_encodestate *state_in)
-{
+int base64_encode_block(const char *plaintext_in, unsigned int length_in, char *code_out, base64_encodestate *state_in) {
     const char *plainchar = plaintext_in;
     const char *const plaintextend = plaintext_in + length_in;
     char *codechar = code_out;
@@ -34,13 +32,10 @@ int base64_encode_block(const char *plaintext_in, unsigned int length_in, char *
 
     result = state_in->result;
 
-    switch (state_in->step)
-    {
-        while (1)
-        {
+    switch (state_in->step) {
+        while (1) {
         case step_A:
-            if (plainchar == plaintextend)
-            {
+            if (plainchar == plaintextend) {
                 state_in->result = result;
                 state_in->step = step_A;
                 return codechar - code_out;
@@ -50,8 +45,7 @@ int base64_encode_block(const char *plaintext_in, unsigned int length_in, char *
             *codechar++ = base64_encode_value(result);
             result = (fragment & 0x003) << 4;
         case step_B:
-            if (plainchar == plaintextend)
-            {
+            if (plainchar == plaintextend) {
                 state_in->result = result;
                 state_in->step = step_B;
                 return codechar - code_out;
@@ -61,8 +55,7 @@ int base64_encode_block(const char *plaintext_in, unsigned int length_in, char *
             *codechar++ = base64_encode_value(result);
             result = (fragment & 0x00f) << 2;
         case step_C:
-            if (plainchar == plaintextend)
-            {
+            if (plainchar == plaintextend) {
                 state_in->result = result;
                 state_in->step = step_C;
                 return codechar - code_out;
@@ -80,12 +73,10 @@ int base64_encode_block(const char *plaintext_in, unsigned int length_in, char *
     return codechar - code_out;
 }
 
-int base64_encode_blockend(char *code_out, base64_encodestate *state_in)
-{
+int base64_encode_blockend(char *code_out, base64_encodestate *state_in) {
     char *codechar = code_out;
 
-    switch (state_in->step)
-    {
+    switch (state_in->step) {
     case step_B:
         *codechar++ = base64_encode_value(state_in->result);
         *codechar++ = '=';
