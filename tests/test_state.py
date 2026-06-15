2e6bb33777e72e119619db48ca0862ebdf0a17bc
jbjd <jimdemetris@yahoo.com>
2026-06-14 20:40:02 -0500

    Depreacate: Temporary deprecate of rotate until I can do zoom overhaul

--- tests/test_state.py
+++ tests/test_state.py
@@ -1,6 +1,6 @@
 """Tests for the ZoomState class."""
 
-from image_viewer.constants import Rotation, ZoomDirection
+from image_viewer.constants import ZoomDirection
 from image_viewer.image.state import ImageState
 
 
@@ -9,38 +9,28 @@ def test_try_update_state() -> None:
     state = ImageState()
     state.zoom_level_max = 1
 
-    assert not state.try_update(ZoomDirection.OUT, None)
+    assert not state.try_update(ZoomDirection.OUT)
     assert state.zoom_level == 0
 
-    assert state.try_update(ZoomDirection.IN, Rotation.DOWN)
-    assert state.zoom_level == 1
-    assert state.orientation == Rotation.DOWN
-
-    assert not state.try_update(ZoomDirection.IN, None)
+    assert state.try_update(ZoomDirection.IN)
     assert state.zoom_level == 1
 
-    assert state.try_update(ZoomDirection.OUT, None)
+    assert not state.try_update(ZoomDirection.IN)
+    assert state.zoom_level == 1
+
+    assert state.try_update(ZoomDirection.OUT)
     assert state.zoom_level == 0
-
-    # Mypy bug? 'Literal[Rotation.DOWN]' non-overlaps 'Literal[Rotation.LEFT]'
-    assert state.try_update(None, Rotation.LEFT)
-    assert state.orientation == Rotation.LEFT  # type: ignore[comparison-overlap]
-
-    assert not state.try_update(ZoomDirection.OUT, Rotation.LEFT)
-    assert state.orientation == Rotation.LEFT
 
 
 def test_reset() -> None:
     state = ImageState()
     state.zoom_level += 1
     state.zoom_level_max += 2
-    state.orientation = Rotation.RIGHT
-    state.zoom_rotate_allowed = not state.zoom_rotate_allowed
+    state.zoom_allowed = not state.zoom_allowed
 
     state.reset()
 
     default_state = ImageState()
     assert state.zoom_level == default_state.zoom_level
     assert state.zoom_level_max == default_state.zoom_level_max
-    assert state.orientation == default_state.orientation
-    assert state.zoom_rotate_allowed is default_state.zoom_rotate_allowed
+    assert state.zoom_allowed is default_state.zoom_allowed
