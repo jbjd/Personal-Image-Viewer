15f90a87bc63964eb713dddf0ac8fa9ee358e46e
jbjd <jimdemetris@yahoo.com>
2026-06-19 23:56:54 -0500

    Improvement: Adjust import order so standalone import errors will log to file

--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 import sys
 
 if __name__ == "__main__" and len(sys.argv) > 1:
-    from image_viewer.exceptions import exception_hook
-    from image_viewer.viewer import ViewerApp
-
     if not __debug__:
         import os
 
+        from image_viewer.exceptions import exception_hook
+
         os.chdir(os.path.dirname(sys.argv[0]))
         sys.excepthook = exception_hook
+
+    from image_viewer.viewer import ViewerApp
 
     ViewerApp(sys.argv[1]).start()
