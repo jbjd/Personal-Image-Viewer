80f24f2008973df86bbd6dd380885066d681300e
jbjd <jimdemetris@yahoo.com>
2026-07-04 18:23:52 -0500

    Improvement: Reduce base64 overhead

--- image_viewer/c_extensions/b64/cencode.c
+++ image_viewer/c_extensions/b64/cencode.c
@@ -12,7 +12,6 @@ Taken from https://github.com/BuLogics/libb64/tree/master with slight edits
 void base64_init_encodestate(base64_encodestate *state_in) {
     state_in->step = step_A;
     state_in->result = 0;
-    state_in->stepcount = 0;
 }
 
 char base64_encode_value(char value_in) {
@@ -64,8 +63,6 @@ int base64_encode_block(const char *plaintext_in, unsigned int length_in, char *
             *codechar++ = base64_encode_value(result);
             result = (fragment & 0x03f) >> 0;
             *codechar++ = base64_encode_value(result);
-
-            ++(state_in->stepcount);
         }
     }
 
