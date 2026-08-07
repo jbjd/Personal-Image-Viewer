3d73355e4988b0700b4957dceba71a882f375d7e
jbjd <jimdemetris@yahoo.com>
2026-08-06 22:44:43 -0500

    Cleanup

--- image_viewer/c_extensions/b64/base64.c
+++ image_viewer/c_extensions/b64/base64.c
@@ -11,40 +11,37 @@ void base64_encode(const char *input, unsigned int input_size, char *encoded_out
     char current_char;
     char *output_position = encoded_out;
 
-    char result = 0;
-
     while (1) {
         if (input_position == input_end) {
-            goto end;
+            break;
         }
         current_char = *input_position++;
-        result = (current_char & 0x0fc) >> 2;
-        *output_position++ = _base64_encode_char(result);
-        result = (current_char & 0x003) << 4;
+        char fragment = (current_char & 0x0fc) >> 2;
+        *output_position++ = _base64_encode_char(fragment);
+        fragment = (current_char & 0x003) << 4;
 
         if (input_position == input_end) {
-            *output_position++ = _base64_encode_char(result);
+            *output_position++ = _base64_encode_char(fragment);
             *output_position++ = '=';
             *output_position++ = '=';
-            goto end;
+            break;
         }
         current_char = *input_position++;
-        result |= (current_char & 0x0f0) >> 4;
-        *output_position++ = _base64_encode_char(result);
-        result = (current_char & 0x00f) << 2;
+        fragment |= (current_char & 0x0f0) >> 4;
+        *output_position++ = _base64_encode_char(fragment);
+        fragment = (current_char & 0x00f) << 2;
 
         if (input_position == input_end) {
-            *output_position++ = _base64_encode_char(result);
+            *output_position++ = _base64_encode_char(fragment);
             *output_position++ = '=';
-            goto end;
+            break;
         }
         current_char = *input_position++;
-        result |= (current_char & 0x0c0) >> 6;
-        *output_position++ = _base64_encode_char(result);
-        result = current_char & 0x03f;
-        *output_position++ = _base64_encode_char(result);
+        fragment |= (current_char & 0x0c0) >> 6;
+        *output_position++ = _base64_encode_char(fragment);
+        fragment = current_char & 0x03f;
+        *output_position++ = _base64_encode_char(fragment);
     }
 
-end:
     *output_position = '\0';
 }
